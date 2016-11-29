0cecba28c2853b98d07b2d56295b06962f08a9ba
zhangyuchen <zhangyuchen@wepiao.com>
2016-11-29 10:57:28 +0800

    确保数组下标不越界

--- bitmap/bitmap.go
+++ bitmap/bitmap.go
@@ -6,27 +6,29 @@ import (
 )
 
 type BitMap32 struct {
-	a []uint32
+	a    []uint32
+	size uint32
 }
 
 func NewBitMap32(size int) *BitMap32 {
 	return &BitMap32{
-		a: make([]uint32, size),
+		a:    make([]uint32, size),
+		size: uint32(size),
 	}
 }
 
 func (bm32 *BitMap32) Set(i uint32) {
 	// i >> 5 => i / 32, 落在哪个槽里
 	// i & 0x1F => i % 32
-	bm32.a[i>>5] |= 1 << (i & 0x1F)
+	bm32.a[(i>>5)%bm32.size] |= 1 << (i & 0x1F)
 }
 
 func (bm32 *BitMap32) Clear(i uint32) {
-	bm32.a[i>>5] &= ^(1 << (i & 0x1F))
+	bm32.a[(i>>5)%bm32.size] &= ^(1 << (i & 0x1F))
 }
 
 func (bm32 *BitMap32) In(i uint32) bool {
-	return bm32.a[i>>5]&(1<<(i&0x1F)) > 0
+	return bm32.a[(i>>5)%bm32.size]&(1<<(i&0x1F)) > 0
 }
 
 func (bm32 *BitMap32) String() string {
@@ -41,27 +43,29 @@ func (bm32 *BitMap32) String() string {
 }
 
 type BitMap64 struct {
-	a []uint64
+	a    []uint64
+	size uint64
 }
 
 func NewBitMap64(size int) *BitMap64 {
 	return &BitMap64{
-		a: make([]uint64, size),
+		a:    make([]uint64, size),
+		size: uint64(size),
 	}
 }
 
 func (bm64 *BitMap64) Set(i uint64) {
 	// i >> 6 => i / 64, 落在哪个槽里
 	// i & 0x3F => i % 64
-	bm64.a[i>>6] |= 1 << (i & 0x3F)
+	bm64.a[(i>>6)%bm64.size] |= 1 << (i & 0x3F)
 }
 
 func (bm64 *BitMap64) Clear(i uint64) {
-	bm64.a[i>>6] &= ^(1 << (i & 0x3F))
+	bm64.a[(i>>6)%bm64.size] &= ^(1 << (i & 0x3F))
 }
 
 func (bm64 *BitMap64) In(i uint64) bool {
-	return bm64.a[i>>6]&(1<<(i&0x3F)) > 0
+	return bm64.a[(i>>6)%bm64.size]&(1<<(i&0x3F)) > 0
 }
 
 func (bm64 *BitMap64) String() string {
